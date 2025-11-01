ba96886fa00d1b52c25a6962e98ca1bdd7cfdab0
jxfong357 <jxfong357@gmail.com>
2025-11-01 02:52:10 +0000

    fix bug

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,7 @@ const nextConfig = {
     unoptimized: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+    // This will prevent the ESLint error from stopping the build.
     ignoreDuringBuilds: true,
   },
 }

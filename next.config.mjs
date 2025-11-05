8b6e07814520a10aff68c6f5decd0f51115ea3b2
jxfong357 <jxfong357@gmail.com>
2025-11-05 14:53:53 +0000

    yw debug

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
     // This will prevent the ESLint error from stopping the build.
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: ["3000-firebase-codenection2025git-1758078224819.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev"],
+  },
 }
 
 export default nextConfig
